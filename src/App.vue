3c528f2b20970eb8e59615477ba072c972af4121
Ilen <ilen8vin@gmail.com>
2021-03-23 00:12:09 +0700

    [Test] testing git

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <MyButton v-on:added="addIsCalled()" state="add"></MyButton>
+        <MyButton v-on:deleted="deleteIsCalled()" state="delete"></MyButton>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+    methods: {
+      addIsCalled() {
+        console.log('added');
+    },
+      deleteIsCalled() {
+        console.log('deleted');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+}
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <button :class="state" v-on:click="clickButton()">
+        <span v-if="state == 'delete'">Delete</span>
+        <span v-else>Add</span>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Mybutton",
+    props: ['state'],
+    emits: ["added", "deleted"],
+    data() {
+        return {
+            //state: 'delete' //delete or add
+        }
+    },
+    methods: {
+        clickButton() {
+            if(this.state == "add") {
+                this.$emit("added");
+            } else {
+                this.$emit("deleted")
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        border: 1px solid black;
+    }
+    .delete {
+        background-color: red;
+    }
+    .add {
+        background-color: violet;
+    }
+</style>
